ff7e33349f8137ff7ce66ea1faee2f142afe2033
Darsh-Patel-26 <darsh.git.26@gmail.com>
2024-12-19 13:07:18 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Expose the app on all network interfaces
+    port: process.env.PORT || 5173,  // Use Render's port if available, otherwise default to 5173
+  },
 })
